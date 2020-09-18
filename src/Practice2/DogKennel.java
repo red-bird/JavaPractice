fabb68d0c0d8ba1a0624a2029165d359ea05af05
Redbird <redbird8941@gmail.com>
2020-09-18 05:42:39 +0300

    DogKennel complete

---
+++ src/Practice2/DogKennel.java
@@ -0,0 +1,68 @@
+package Practice2;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Dog
+{
+    String name;
+    int age;
+    void setName(String name)
+    {
+        this.name = name;
+    }
+    void setAge(int age)
+    {
+        this.age = age;
+    }
+    String getName()
+    {
+        return name;
+    }
+    int getAge()
+    {
+        return age;
+    }
+    Dog(String name, int age)
+    {
+        setAge(age);
+        setName(name);
+    }
+    int getHumanAge()
+    {
+        return getAge()*7;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+
+public class DogKennel {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n;
+        n = in.nextInt();
+
+        ArrayList dogs = new ArrayList(n);
+
+        for (int i = 0; i < n; ++i)
+        {
+            String name;
+            int age;
+            name = in.next();
+            age = in.nextInt();
+            Dog dog = new Dog(name, age);
+            dogs.add(dog);
+        }
+
+        for (int i = 0; i < n; ++i)
+        {
+            System.out.println(dogs.get(i).toString());
+        }
+    }
+}
