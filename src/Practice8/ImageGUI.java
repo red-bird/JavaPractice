2a731558e14a15c53ec7a473c89879f09dc3a0d0
lamossus <zhora.polyakov95@mail.ru>
2020-10-02 20:45:49 +0300

    complete exercise2

---
+++ src/Practice8/ImageGUI.java
@@ -0,0 +1,36 @@
+package Practice8;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Scanner;
+
+public class ImageGUI extends JFrame {
+    public ImageGUI(String path){
+        setTitle("ImageGUI");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        Dimension dim = kit.getScreenSize();
+        setSize(dim.width, dim.height);
+        setResizable(true);
+        setVisible(true);
+
+        Graphics grph;
+        Image img;
+
+        img = new ImageIcon(path).getImage();
+
+        grph = getGraphics();
+        while(true) {
+            grph.drawImage(img, dim.width-img.getWidth(null)/2, dim.height-img.getHeight(null)/2, null);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter rood to img");
+        String path = new String();
+        Scanner sc = new Scanner(System.in);
+        path = sc.nextLine();
+        new ImageGUI(path);
+    }
+}
+
